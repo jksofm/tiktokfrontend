ea2411adb6076b1b4246483777f7464ad60f1b86
jksofm <lequochuypy1998@gmail.com>
2022-06-30 17:44:34 +0700

    LayoutUpdate

--- src/App.js
+++ src/App.js
@@ -8,13 +8,12 @@ function App() {
       <div className="App">
         <Routes>
           {publicRoutes.map(function (route, index) {
-            
             const Page = route.component;
             let Layout = DefaultLayout;
-            if(route.layout){
-                Layout = route.layout
-            }else if( route.layout === null){
-                Layout = Fragment;
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
             }
             return (
               <Route
