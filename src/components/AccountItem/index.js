5daf2327c9653b73e4c860f8d3b4db95f8c4f8e7
jksofm <lequochuypy1998@gmail.com>
2022-07-03 16:42:35 +0700

    HeaderUIupdate

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,26 @@
+import styles from './AccountItem.module.scss';
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+function AccountItem() {
+  return (
+    <div className={cx('wrapper')}>
+      <img
+        className={cx('avatar')}
+        src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/ddcb57a7bcd8bf0fc01c18338b2caf59~c5_300x300.webp?x-expires=1656990000&x-signature=%2Bf1nLwjIuj6SHe38DSGlxI4eMnY%3D"
+        alt="Hoa"
+      />
+      <div className={cx('info')}>
+        <h4 className={cx('name')}>
+          <span>Nguyen Dao Phuong Hoa</span>
+          <FontAwesomeIcon className={cx('check-icon')} icon={faCheckCircle} />
+        </h4>
+        <span className={cx('username')}>nguyenvana</span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountItem;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,15 +1,79 @@
+import { useEffect, useState } from 'react';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
+import images from '~/assets/images';
+import Tippy from '@tippyjs/react/headless';
+import 'tippy.js/dist/tippy.css';
+import AccountItem from '~/components/AccountItem';
+import Button from '~/components/Button';
 
 const cx = classNames.bind(styles);
 
-
 function Header() {
-    return <header className={cx('wrapper')} >
-        <div className={cx('inner')}>
-
+  const [searchResults, setSearchResults] = useState([]);
+  useEffect(() => {
+    setTimeout(() => {
+      setSearchResults([]);
+    }, 3000);
+  }, []);
+  return (
+    <header className={cx('wrapper')}>
+      <div className={cx('inner')}>
+        <div className={cx('logo')}>
+          <img src={images.logo} alt="logo" />
         </div>
+        <Tippy
+          interactive={true}
+          visible={searchResults.length > 0}
+          render={(attrs) => (
+            <div className={cx('search-results')} tabIndex="-1" {...attrs}>
+              <PopperWrapper>
+                <h4 className={cx('search-title')}>Accounts</h4>
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+              </PopperWrapper>
+            </div>
+          )}
+        >
+          <div className={cx('search')}>
+            <input placeholder="Search accounts and videos..." spellCheck={false} />
+
+            <span></span>
+
+            <button className={cx('search-btn')}>
+              {/*search */}
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+
+            <button className={cx('clear')}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+            <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+          </div>
+        </Tippy>
+
+        <div className={cx('actions')}>
+          <Button text onClick={console.log('Hi')}>
+            Upload
+          </Button>
+          <Button
+            // leftIcon={<FontAwesomeIcon icon={faSpinner} />}
+            primary
+            // className={cx('custom-loggin')}
+            // rounded
+            onClick={console.log('Hi')}
+          >
+            Log in
+          </Button>
+        </div>
+      </div>
     </header>
+  );
 }
 
 export default Header;
